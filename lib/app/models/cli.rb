fb2e355457c70e18919aeaa58f6f6398f5395c7f
thecodeplanner <thecodeplanner@gmail.com>
2020-11-10 11:57:05 -0500

    more updates

--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -7,6 +7,9 @@ class CLI
     @@user = nil
     @@plants = []
     @@gardenplant = [] 
+    @@prompt = TTY::Prompt.new
+
+
     def load_plants
         @@plants = Plant.all.map{|plant|plant}
     end
@@ -22,9 +25,8 @@ class CLI
 
     
     def greet
-        prompt = TTY::Prompt.new
         system("clear")
-        welcome = prompt.select("Welcome to Zen Garden!") do |menu|
+        welcome = @@prompt.select("Welcome to Zen Garden!") do |menu|
             menu.choice 'Sign Up'
             menu.choice 'Login'
             menu.choice 'Quit'
@@ -48,9 +50,8 @@ class CLI
     end
 
     def menu
-        prompt = TTY::Prompt.new
         system("clear")
-        option = prompt.select("What would you like to do today?") do |menu|
+        option = @@prompt.select("What would you like to do today?") do |menu|
             menu.choice 'Plant new plants in my garden'
             menu.choice 'View my garden'
             menu.choice 'Harvest my garden'
@@ -58,10 +59,9 @@ class CLI
         end
 
         if option == 'Plant new plants in my garden'
-            menu_2
-        elsif option == 'View my garden' 
-            puts "#{@@user.plants.map{|plant|plant.name}}"
-            
+            self.menu_2
+        elsif option == 'View my garden'
+            self.view_my_garden
         elsif option == 'Harvest my garden'
             @@user.plants.destroy_all
             @@user.save
@@ -74,9 +74,8 @@ class CLI
     end
 
     def menu_2
-        prompt = TTY::Prompt.new
         system("clear")
-        plant_selection = prompt.select("What would you like to plant today?") do |menu|
+        plant_selection = @@prompt.select("What would you like to plant today?") do |menu|
             menu.choice 'Fern'
             menu.choice 'Cactus'
             menu.choice 'Flower'
@@ -85,22 +84,65 @@ class CLI
         if plant_selection == 'Fern'
             plant = Plant.all.find_by(name: "fern")
             @@user.plant_plant_in_garden(plant)
+            puts "Nice choice! Ferns are known for their calming properties." 
+            
         elsif plant_selection == 'Cactus'
             plant = Plant.all.find_by(name: "cactus")
             @@user.plant_plant_in_garden(plant)
+            puts "Great job! Be careful of their spikes!"
+
         elsif plant_selection == 'Flower'
             plant = Plant.all.find_by(name: "flower")
             @@user.plant_plant_in_garden(plant)
-            ##create a back option
+            puts "It's always a good idea to wake up and smell the flowers!"
+            
         end
     end
 
-    
-    # def add_favorite_snack(recipe)
-    #     FavoriteSnack.create(snacker_id: self.id, recipe_id: recipe.id, name: recipe.name)
+    # def menu_3
+    #     selection = @@prompt.select("What would you like to do next") do |menu|
+    #         menu.choice 'View My Garden'
+    #         menu.choice 'Plant Another Plant'
+    #         menu.choice 'Main menu'
+    #         menu.choice 'Quit'
+    #     end
+
     # end
+
+    def view_my_garden
+        system("clear") 
+        puts "Here are all of your plants:"  
+        x = 1
+        plants = @@user.plants.map{|plant|plant.name}
+        plants.each do |plant|
+            puts "#{x}. #{plant}"
+            x += 1
+        end
+                    
+        option2 = @@prompt.select("Your garden is looking fabulous! What would you like to do next?") do |menu|
+            menu.choice 'Water My Plants'
+            menu.choice "Check My Plants' Status' "
+            menu.choice 'Plant More plants'
+            menu.choice 'Return to Main Menu'
+            menu.choice 'Quit'
+        end
+
+
+        if option2 == 'Water My Plants'
+            
+        elsif option2 == "Check My Plants' Status' "
+            
+        elsif option2 == 'Plant More plants'
+            self.menu_2
+        elsif option2 == 'Return to Main Menu'
+            self.menu
+        elsif option2 == 'Quit'
+            puts "Bye!"
+        end
+    end
 
 
 
 end
+
 
